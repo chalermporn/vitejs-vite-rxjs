222ac9086d184c3291f5acf7d6c2a142ec70ae7f
chalermporn <bird.urights@gmail.com>
2022-09-18 15:06:13 +0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     Test Rxjs Ajax
     <ul>
-      <li v-for="item in frameworks" :key="item">{{ item.login }}</li>
+      <li v-for="item in frameworks" :key="item">{{ item.name }}</li>
     </ul>
   </div>
 </template>
@@ -15,10 +15,41 @@ import { map, catchError, of } from 'rxjs'
 export default defineComponent({
     setup() {
         let frameworks: Ref<any[]> = ref([])
-        const obs$ = ajax('https://api.github.com/users?per_page=10').pipe(
-            map(userResponse => {
-                console.log('users: ', userResponse)
-                const { response } = userResponse
+        // const obs$ = ajax('https://api.github.com/users?per_page=10').pipe(
+        //     map(userResponse => {
+        //         console.log('users: ', userResponse)
+        //         const { response } = userResponse
+        //         return response
+        //     }),
+        //     catchError(error => {
+        //         console.log('error: ', error)
+        //         return of(error)
+        //     })
+        // )
+
+        // onMounted(() => {
+        //     obs$.subscribe({
+        //         next: value => {
+        //             console.log(value)
+        //             frameworks.value = value
+        //         },
+        //         error: err => console.log(err)
+        //     })
+        // })
+        // return {
+        //     frameworks,
+        // }
+
+
+         const urlPath = `https://api.github.com/search/topics?q=vue`
+
+        const users: Observable<any> = ajax({
+            url: urlPath,
+            method: 'GET',
+        }).pipe(
+            map(resp => {
+                console.log('users: ', resp)
+                const { response } = resp
                 return response
             }),
             catchError(error => {
@@ -28,10 +59,10 @@ export default defineComponent({
         )
 
         onMounted(() => {
-            obs$.subscribe({
+            users.subscribe({
                 next: value => {
                     console.log(value)
-                    frameworks.value = value
+                    frameworks.value = value.items
                 },
                 error: err => console.log(err)
             })
